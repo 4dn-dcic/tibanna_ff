8b37d5ef00621ded249418df558a174c03f552f2
SooLee <duplexa@gmail.com>
2019-10-21 14:35:34 -0400

    upload option added to pf post for test

--- tests/tibanna/zebra/conftest.py
+++ tests/tibanna/zebra/conftest.py
@@ -48,7 +48,8 @@ def post_new_fastqfile(key, upload_file=None):
 
 
 @valid_env
-def post_new_processedfile(file_format, key, extra_file_formats=None, **kwargs):
+def post_new_processedfile(file_format, key, extra_file_formats=None,
+                           upload_file=None, extension=None, **kwargs):
     if extra_file_formats:
         extra_files = [{'file_format': ef} for ef in extra_file_formats]
     else:
@@ -57,6 +58,11 @@ def post_new_processedfile(file_format, key, extra_file_formats=None, **kwargs):
                                    extra_files=extra_files,
                                    other_fields=kwargs).as_dict()
     res = ff_utils.post_metadata(new_pf, 'FileProcessed', key=key)
+    if upload_file:
+        uuid = res['@graph'][0]['uuid']
+        accession = res['@graph'][0]['accession']
+        upload_key = uuid + '/' + accession + '.' + extension
+        boto3.client('s3').upload_file(upload_file, DEV_FILE_BUCKET, upload_key)
     return res['@graph'][0]['uuid']
 
 
