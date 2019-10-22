b0b68651e9d7a78165043c305af02279b60832b7
SooLee <duplexa@gmail.com>
2019-10-22 15:47:26 +0000

    added bwa-mem test for post-deployment test

--- tests/post-deployment/test_pony.py
+++ tests/post-deployment/test_pony.py
@@ -5,6 +5,7 @@ import time
 from tibanna_4dn.core import API
 from tibanna_4dn.vars import DEV_SFN
 
+
 JSON_DIR = 'test_json/pony/'
 
 
@@ -26,6 +27,18 @@ def test_fastqc():
     assert 'jobid' in res
     assert 'exec_arn' in res['_tibanna']
     time.sleep(360)
+    assert api.check_status(res['_tibanna']['exec_arn']) == 'SUCCEEDED'
+    postrunjson = json.loads(api.log(job_id=res['jobid'], postrunjson=True))
+    assert 'status' in postrunjson['Job']
+    assert postrunjson['Job']['status'] == '0'
+
+
+def test_bwa():
+    api = API()
+    res = api.run_workflow(os.path.abspath(os.path.join(JSON_DIR, 'bwa-mem.json')), sfn=DEV_SFN)
+    assert 'jobid' in res
+    assert 'exec_arn' in res['_tibanna']
+    time.sleep(60 * 16)  # runs for 15 min
     assert api.check_status(res['_tibanna']['exec_arn']) == 'SUCCEEDED'
     postrunjson = json.loads(api.log(job_id=res['jobid'], postrunjson=True))
     assert 'status' in postrunjson['Job']
