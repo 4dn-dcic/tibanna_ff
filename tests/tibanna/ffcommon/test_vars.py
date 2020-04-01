1bc8db5a9382710c00a20d5663f235f1f7774644
SooLee <duplexa@gmail.com>
2020-04-01 17:57:51 +0000

    test for bucketname

---
+++ tests/tibanna/ffcommon/test_vars.py
@@ -0,0 +1,13 @@
+from tibanna_ffcommon.vars import BUCKET_NAME
+
+def test_bucket_name():
+    bucket = BUCKET_NAME('fourfront-green', 'FileProcessed')
+    assert bucket == 'elasticbeanstalk-fourfront-webprod-wfoutput'
+
+def test_bucket_name2():
+    bucket = BUCKET_NAME('fourfront-blue', 'FileReference')
+    assert bucket == 'elasticbeanstalk-fourfront-webprod-files'
+
+def test_bucket_name3():
+    bucket = BUCKET_NAME('fourfront-cgap', 'FileFastq')
+    assert bucket == 'elasticbeanstalk-fourfront-cgap-files'
