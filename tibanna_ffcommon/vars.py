49c2037a3a5adc52eafd030f05f7b1a213f33f1e
SooLee <duplexa@gmail.com>
2021-06-14 14:29:15 -0400

    bucket names from s3utils

--- tibanna_ffcommon/vars.py
+++ tibanna_ffcommon/vars.py
@@ -10,12 +10,31 @@ UPDATE_COST_LAMBDA_NAME = 'update_cost'
 START_RUN_LAMBDA_NAME = 'start_run'
 UPDATE_FFMETA_LAMBDA_NAME = 'update_ffmeta'
 
+GLOBAL_BUCKET_ENV = of.environ.get('GLOBAL_BUCKET_ENV', '')
+
+
+# cached bucket names (internally used by function BUCKET_NAME
+_BUCKET_NAME_PROCESSED_FILES = ''
+_BUCKET_NAME_RAW_RILES = ''
+
 
 def BUCKET_NAME(env, filetype):
-    if filetype in ['FileFastq', 'FileReference']:
-        bucket_type = 'files'
+    # use cache
+    if filetype == 'FileProcessed' and _BUCKET_NAME_PROCESSED_FILES:
+        return _BUCKET_NAME_PROCESSED_FILES
+    if filetype in ['FileFastq', 'FileReference'] and _BUCKET_NAME_RAW_RILES:
+        return _BUCKET_NAME_RAW_RILES
+
+    # no cache
+    if env.startswith('fourfront-'):
+        env = env.replace('fourfront-', '')
+    s3 = s3Utils(env=env)
+    _BUCKET_NAME_PROCESSED_FILES = s3.outfile_bucket
+    _BUCKET_NAME_RAW_RILES = s3.raw_file_bucket
+
+    if filetype == 'FileProcessed':
+        return _BUCKET_NAME_PROCESSED_FILES
     else:
-        bucket_type = 'wfoutput'
-    if env in ['data', 'staging', 'fourfront-green', 'fourfront-blue']:
-        env = 'fourfront-webprod'
+        return _BUCKET_NAME_RAW_RILES
+
     return 'elasticbeanstalk-%s-%s' % (env, bucket_type)
