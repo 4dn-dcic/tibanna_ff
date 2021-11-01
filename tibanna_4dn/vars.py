c21765ef75b0e1653ef42a5719156501b7367752
William Ronchetti <wrr33@cornell.edu>
2021-11-01 14:53:46 -0400

    bring in new secure ami

--- tibanna_4dn/vars.py
+++ tibanna_4dn/vars.py
@@ -1,5 +1,5 @@
 from tibanna_ffcommon.vars import *
-import os
+
 
 LAMBDA_TYPE = 'pony'
 SFN_TYPE = 'pony'
@@ -20,6 +20,7 @@ HIGLASS_BUCKETS = [BUCKET_NAME('data', 'FileProcessed'),
 DEV_ENV = 'webdev'
 PROD_ENV = 'data'
 
+
 def IAM_BUCKETS(env):
     iam_buckets = [BUCKET_NAME(env, 'FileProcessed'),
                    BUCKET_NAME(env, 'FileFastq'),
@@ -30,4 +31,19 @@ def IAM_BUCKETS(env):
         iam_buckets.append(GLOBAL_BUCKET_ENV)
     return iam_buckets
 
+
 DEV_SFN = 'tibanna_' + SFN_TYPE + '_' + DEV_ENV + '_' + DEV_SUFFIX
+
+
+# Secure Tibanna AMI
+# Note that this means only these regions will work (replicate AMI in main account as needed)
+# Note additionally that these AMI's all must be configured to be launchable by our AWS
+# accounts
+AMI_PER_REGION = {
+    'us-east-1': 'ami-00ad035048da98fc2',
+    'us-east-2': 'ami-0afbf1ab3268ddf17',
+}
+if AWS_REGION not in AMI_PER_REGION:
+    logger.warning("Secure Tibanna AMI for region %s is not available." % AWS_REGION)
+    raise Exception('')
+AMI_ID = AMI_PER_REGION.get(AWS_REGION, '')
