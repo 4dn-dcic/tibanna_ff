c4a8b4cb98318103e6d43a5d45efa6f9f6a88bad
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2021-11-02 11:55:52 -0400

    Fix costupdater (#47)

--- tibanna_cgap/lambdas/update_cost.py
+++ tibanna_cgap/lambdas/update_cost.py
@@ -1,6 +1,4 @@
-from tibanna.lambdas import update_cost_awsem as update_cost
-from tibanna.lambdas.update_cost_awsem import config
-from tibanna_ffcommon.exceptions import exception_coordinator
+from tibanna.lambdas.update_cost_awsem import handler as _handler, config
 from tibanna_cgap.vars import LAMBDA_TYPE
 
 
@@ -8,4 +6,4 @@ config['function_name'] = 'update_cost_' + LAMBDA_TYPE
 
 
 def handler(event, context):
-    return update_cost(event)
+    return _handler(event)
