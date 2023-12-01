87cc1ee4f31029249008b5b3e562518008b07533
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2023-12-01 14:51:36 -0500

    Add SubmittedFile to vars

--- tibanna_ffcommon/vars.py
+++ tibanna_ffcommon/vars.py
@@ -65,7 +65,7 @@ def BUCKET_NAME(env, filetype):
     global _BUCKET_NAME_CWL
 
     processed_file_types = ['FileProcessed', 'OutputFile']
-    raw_file_types = ['FileFastq', 'FileReference', 'ReferenceFile' 'FileMicroscopy', 'FileSubmitted']
+    raw_file_types = ['FileFastq', 'FileReference', 'ReferenceFile', 'SubmittedFile', 'FileMicroscopy', 'FileSubmitted']
 
     # use cache
     if filetype in processed_file_types and env in _BUCKET_NAME_PROCESSED_FILES:
